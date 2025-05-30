2e0cf9e29bfb1377e896eb3fdb230a481545b200
Aleksandr Shabalin <aleks.shbln@gmail.com>
2025-05-30 21:56:07 +0200

    Initial commit of a backend

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+let urls = [
+  { original: "http://example.com", short: "https://shortenb.com/abc123" }
+]
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/urls', (req, res) => {
+  res.json(urls)
+})
+
+app.post('/generate', (req, res) => {
+  let body = req.body
+  console.log(body)
+  urls.push({ 
+    original: body.url, 
+    short: "https://shortenb.com/" + new Date().getTime() 
+  })
+  res.json({ 
+    date: new Date(), 
+    short_url: urls[urls.length - 1].short 
+  })
+  // res.send({ short_url: "https://shortenb.io/abc123"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
